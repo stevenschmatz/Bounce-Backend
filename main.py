a14c55c6d1975a0835b559d2d761ef1cef91592f
Steven Schmatz <stevenschmatz@gmail.com>
2014-12-26 01:15:32 -0500

    test

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app.config['DEBUG'] = True
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World! This is a test.'
+    return 'Hello World! This is a test. Lol!'
 
 
 @app.errorhandler(404)
